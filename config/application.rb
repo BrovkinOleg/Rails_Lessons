45329315d0f2812f840469947dc70f26ceb44532
BrovkinOleg <boxline@yandex.ru>
2019-08-27 11:37:01 +0300

    "add version.rb as a module"

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,14 @@
 require_relative 'boot'
 
 require 'rails/all'
+require_relative 'version'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TestGuru
+  include TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
---
+++ config/initializers/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# use the version Configuration.app_version
+module TestGuru
+  class Application < Rails::Application
+    VERSION = '0.1.0'
+  end
+end
+
