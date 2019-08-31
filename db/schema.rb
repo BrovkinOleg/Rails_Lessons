ed1252168dc486e04045f5889fc53010866a97f3
Brovkin Oleg <boxline@yandex.ru>
2019-08-31 10:57:57 +0300

    "change nul constrains and add level default"

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_830_190_953) do
+ActiveRecord::Schema.define(version: 20_190_831_074_813) do
   create_table 'answers', force: :cascade do |t|
-    t.string 'body'
-    t.boolean 'correct'
+    t.string 'body', null: false
+    t.boolean 'correct', default: false, null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.integer 'questions_id'
@@ -23,13 +23,13 @@ ActiveRecord::Schema.define(version: 20_190_830_190_953) do
   end
 
   create_table 'categories', force: :cascade do |t|
-    t.string 'title'
+    t.string 'title', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
 
   create_table 'questions', force: :cascade do |t|
-    t.string 'body'
+    t.string 'body', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.integer 'tests_id'
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20_190_830_190_953) do
   end
 
   create_table 'tests', force: :cascade do |t|
-    t.string 'title'
-    t.integer 'level'
+    t.string 'title', null: false
+    t.integer 'level', default: 0, null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.integer 'categories_id'
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20_190_830_190_953) do
   end
 
   create_table 'users', force: :cascade do |t|
-    t.string 'name'
+    t.string 'name', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
