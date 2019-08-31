e7b05375b1480435bc7c6aad5092f0a7b59dc487
Brovkin Oleg <boxline@yandex.ru>
2019-08-31 10:57:57 +0300

    "change nul constrains and add level default"

--- db/migrate/20190830190953_add_to_all_tables_null_constraints.rb
+++ db/migrate/20190830190953_add_to_all_tables_null_constraints.rb
@@ -1,11 +1,9 @@
 class AddToAllTablesNullConstraints < ActiveRecord::Migration[6.0]
   def change
-    change_column_null(:categories, :title, from: true, to: false)
-    change_column_null(:tests, :title, from: true, to: false)
-    change_column_null(:tests, :level, from: true, to: false)
-    change_column_null(:questions, :body, from: true, to: false)
-    change_column_null(:answers, :body, from: true, to: false)
-    change_column_null(:answers, :correct, from: true, to: false)
-    change_column_null(:users, :name, from: true, to: false)
+    change_column_null(:categories, :title, false)
+    change_column_null(:tests, :title, false)
+    change_column_null(:questions, :body, false)
+    change_column_null(:answers, :body, false)
+    change_column_null(:users, :name, false)
   end
 end
