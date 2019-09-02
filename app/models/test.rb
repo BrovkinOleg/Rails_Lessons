5bc5a141242df79cc3f01f13f8fb466a44fac4fe
Brovkin Oleg <boxline@yandex.ru>
2019-09-02 14:43:38 +0300

    "исправлены замечания по GitHub и методам"

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,7 @@
 class Test < ApplicationRecord
 
   def self.tests_array_show(category)
-    line = Category.find_by(title: category)
-    puts 'Find tests for' + line.title
-    array = Test.where('categories_id == ?', line.id)
-    array = array.order('id DESC')
-    test_titles = []
-    array.each { |test| test_titles << test.title + ' ' + test.level.to_s }
-    puts test_titles
+    my_array = Test.where('title == ?', category)
+    my_array.order('id DESC')
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,52 +7,57 @@ Question.delete_all
 Test.delete_all
 Category.delete_all
 
-Category.create(title: 'Rails')
-Category.create(title: 'HTML')
-Category.create(title: 'JAVA')
+categories = Category.create!([{ title: 'Rails' }, { title: 'JAVA' }, { title: 'JAVA' }]).to_a
 
-Test.create(title: 'Rails', level: 0, categories_id: Category.find_by_title('Rails').id)
-Test.create(title: 'Rails', level: 1, categories_id: Category.find_by_title('Rails').id)
-Test.create(title: 'Rails', level: 2, categories_id: Category.find_by_title('Rails').id)
-Test.create(title: 'Rails', level: 3, categories_id: Category.find_by_title('Rails').id)
-Test.create(title: 'HTML', level: 0, categories_id: Category.find_by_title('HTML').id)
-Test.create(title: 'HTML', level: 1, categories_id: Category.find_by_title('HTML').id)
-Test.create(title: 'HTML', level: 2, categories_id: Category.find_by_title('HTML').id)
-Test.create(title: 'HTML', level: 3, categories_id: Category.find_by_title('HTML').id)
-Test.create(title: 'JAVA', level: 0, categories_id: Category.find_by_title('JAVA').id)
-Test.create(title: 'JAVA', level: 1, categories_id: Category.find_by_title('JAVA').id)
-Test.create(title: 'JAVA', level: 2, categories_id: Category.find_by_title('JAVA').id)
-Test.create(title: 'JAVA', level: 3, categories_id: Category.find_by_title('JAVA').id)
+tests = Test.create!([
+                       { title: 'Rails', level: 0, categories_id: categories[0].id },
+                       { title: 'Rails', level: 1, categories_id: categories[0].id },
+                       { title: 'Rails', level: 2, categories_id: categories[0].id },
+                       { title: 'Rails', level: 3, categories_id: categories[0].id },
+                       { title: 'HTML', level: 0, categories_id: categories[1].id },
+                       { title: 'HTML', level: 1, categories_id: categories[1].id },
+                       { title: 'HTML', level: 2, categories_id: categories[1].id },
+                       { title: 'HTML', level: 3, categories_id: categories[1].id },
+                       { title: 'JAVA', level: 0, categories_id: categories[2].id },
+                       { title: 'JAVA', level: 1, categories_id: categories[2].id },
+                       { title: 'JAVA', level: 2, categories_id: categories[2].id },
+                       { title: 'JAVA', level: 3, categories_id: categories[2].id }
+                     ])
 
-Question.create(body: 'question_01', tests_id: Test.find_by(title: 'Rails', level: 0).id)
-Question.create(body: 'question_02', tests_id: Test.find_by(title: 'Rails', level: 1).id)
-Question.create(body: 'question_03', tests_id: Test.find_by(title: 'Rails', level: 2).id)
-Question.create(body: 'question_04', tests_id: Test.find_by(title: 'Rails', level: 3).id)
-Question.create(body: 'question_05', tests_id: Test.find_by(title: 'HTML', level: 0).id)
-Question.create(body: 'question_06', tests_id: Test.find_by(title: 'HTML', level: 1).id)
-Question.create(body: 'question_07', tests_id: Test.find_by(title: 'HTML', level: 2).id)
-Question.create(body: 'question_08', tests_id: Test.find_by(title: 'HTML', level: 3).id)
-Question.create(body: 'question_09', tests_id: Test.find_by(title: 'JAVA', level: 0).id)
-Question.create(body: 'question_10', tests_id: Test.find_by(title: 'JAVA', level: 1).id)
-Question.create(body: 'question_11', tests_id: Test.find_by(title: 'JAVA', level: 2).id)
-Question.create(body: 'question_12', tests_id: Test.find_by(title: 'JAVA', level: 3).id)
+questions = Question.create!([
+                               { body: 'question_01', tests_id: tests[0].id },
+                               { body: 'question_02', tests_id: tests[1].id },
+                               { body: 'question_03', tests_id: tests[2].id },
+                               { body: 'question_04', tests_id: tests[3].id },
+                               { body: 'question_05', tests_id: tests[4].id },
+                               { body: 'question_06', tests_id: tests[5].id },
+                               { body: 'question_07', tests_id: tests[6].id },
+                               { body: 'question_08', tests_id: tests[7].id },
+                               { body: 'question_09', tests_id: tests[8].id },
+                               { body: 'question_10', tests_id: tests[9].id },
+                               { body: 'question_11', tests_id: tests[10].id },
+                               { body: 'question_12', tests_id: tests[11].id }
+                             ])
 
-Answer.create(body: 'answer_01', questions_id: Question.find_by(body: 'question_01').id)
-Answer.create(body: 'answer_02', questions_id: Question.find_by(body: 'question_02').id)
-Answer.create(body: 'answer_03', questions_id: Question.find_by(body: 'question_03').id)
-Answer.create(body: 'answer_04', questions_id: Question.find_by(body: 'question_04').id)
-Answer.create(body: 'answer_05', questions_id: Question.find_by(body: 'question_05').id)
-Answer.create(body: 'answer_06', questions_id: Question.find_by(body: 'question_06').id)
-Answer.create(body: 'answer_07', questions_id: Question.find_by(body: 'question_07').id)
-Answer.create(body: 'answer_08', questions_id: Question.find_by(body: 'question_08').id)
-Answer.create(body: 'answer_09', questions_id: Question.find_by(body: 'question_09').id)
-Answer.create(body: 'answer_10', questions_id: Question.find_by(body: 'question_10').id)
-Answer.create(body: 'answer_11', questions_id: Question.find_by(body: 'question_11').id)
-Answer.create(body: 'answer_12', questions_id: Question.find_by(body: 'question_12').id)
+Answer.create!([
+                 { body: 'answer_01', questions_id: questions[0].id },
+                 { body: 'answer_02', questions_id: questions[1].id },
+                 { body: 'answer_03', questions_id: questions[2].id },
+                 { body: 'answer_04', questions_id: questions[3].id },
+                 { body: 'answer_05', questions_id: questions[4].id },
+                 { body: 'answer_06', questions_id: questions[5].id },
+                 { body: 'answer_07', questions_id: questions[6].id },
+                 { body: 'answer_08', questions_id: questions[7].id },
+                 { body: 'answer_09', questions_id: questions[8].id },
+                 { body: 'answer_10', questions_id: questions[9].id },
+                 { body: 'answer_11', questions_id: questions[10].id },
+                 { body: 'answer_11', questions_id: questions[10].id }
+               ])
 
-User.create(name: 'Admin')
-User.create(name: 'Sara')
-User.create(name: 'Jack')
-User.create(name: 'Bill')
-User.create(name: 'John')
-
+User.create!([
+               { name: 'Admin' },
+               { name: 'Sara' },
+               { name: 'Jack' },
+               { name: 'Bill' },
+               { name: 'John' }
+             ])
