b1d7bfd264c044d313b2cbbca8a47b5e0ccad4c2
Brovkin Oleg <boxline@yandex.ru>
2019-09-02 15:02:13 +0300

    'подчистил все лишнее в файле user.rb'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,5 @@
 class User < ApplicationRecord
 
-  # def tests_list_show(complexity_level)
-  #   test_array = Test.where('level == ?', complexity_level)
-  #   test_titles = []
-  #   test_array.each do |test|
-  #     test_titles << test.title.to_s + '_' + test.level.to_s
-  #   end
-  #   puts "Tests list for complexity_level = #{complexity_level} :"
-  #   puts test_titles
-  # end
   def tests_list_show(complexity_level)
     Test.where('level == ?', complexity_level)
   end
