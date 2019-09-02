40a7c2241ea03ae6aa4bee1b6025767deff0be4e
Brovkin Oleg <boxline@yandex.ru>
2019-09-02 12:27:31 +0300

    "слил ветку Lesson_03 и master. Заново добавил Lesson_04 "

--- db/migrate/20190829195857_create_users.rb
+++ db/migrate/20190829195857_create_users.rb
@@ -6,14 +6,4 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps
     end
   end
-
-  def tests_list_show(complexity_level)
-    test_array = Test.where('level == ?', complexity_level)
-    test_titles = []
-    test_array.each do |test|
-      test_titles << test.title.to_s + '_' + test.level.to_s
-    end
-    puts "Tests list for complexity_level = #{complexity_level} :"
-    puts test_titles
-  end
 end
