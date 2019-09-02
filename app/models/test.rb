3a180418274d0429d71db0dfd55431a9dbe97e8b
Brovkin Oleg <boxline@yandex.ru>
2019-09-02 14:43:38 +0300

    "исправлены замечания по GitHub и методам"

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,6 @@
 class Test < ApplicationRecord
 
   def self.tests_array_show(category)
-    line = Category.find_by(title: category)
-    puts 'Find tests for' + line.title
-    array = Test.where('categories_id == ?', line.id)
-    array = array.order('id DESC')
-    test_titles = []
-    array.each { |test| test_titles << test.title + ' ' + test.level.to_s }
-    puts test_titles
+    Test.where('title == ?', category).order('id DESC')
   end
 end
