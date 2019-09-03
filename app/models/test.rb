e449ded4ed9e67505c0307fadf1466f62b6a827e
Brovkin Oleg <boxline@yandex.ru>
2019-09-03 18:35:00 +0300

    'file user.rb change to hash style'

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,6 @@ class Test < ApplicationRecord
 
   def self.tests_array_show(category)
     sql = 'JOIN categories ON categories.id = tests.categories_id'
-    joins(sql).where('categories.title = :cat', cat: category).order('tests.id DESC').pluck(:title)
+    joins(sql).where(categories: { title: category }).order('tests.id DESC').pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 
   def tests_list_show(level)
     sql = 'JOIN users_tests ON tests.id = users_tests.tests_id'
-    Test.joins(sql).where('level = :lev AND users_tests.users_id = :sym', lev: level, sym: id)#.pluck(:title)
+    Test.joins(sql).where(level: level, users_tests: { users_id: id })
   end
 end
