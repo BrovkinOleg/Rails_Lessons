1136632aa47abdc47e8186df49544ca392ae3be8
Brovkin Oleg <boxline@yandex.ru>
2019-09-07 12:48:13 +0300

    'add all validates methods for Lesson_06'

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
   validates :body, presence: true
+  validate :answers_number
+
   scope :right_answer, -> { where(correct: true) }
+
+  private
+
+  def answers_number
+    count = question.answers.count
+    errors.add(:question, 'wrong number of answers') unless (1..4).include?count
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,8 +5,9 @@ class Test < ApplicationRecord
   has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users
 
-  validates :title, presence: true
-  validates :level, numericality: { only_integer: true }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true,
+                    uniqueness: { scope: :level, message: 'Only one test with this name and level' }
 
   scope :easy, -> { where(level: [0, 1]) }
   scope :middle, -> { where(level: 2..4) }
