b9c14636ac6175b0368767419ad1f707102d06f1
Brovkin Oleg <boxline@yandex.ru>
2019-09-09 20:33:23 +0300

    'add rescue with question was not found'

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,7 @@
 class QuestionsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   # http://localhost:3000/tests/:test_id/questions
   def show
     render json: { questions_for_test: Question.where(test_id: params[:test_id]) }
@@ -10,7 +12,9 @@ class QuestionsController < ApplicationController
     render json: { one_question_for_test: Question.where(test_id: params[:test_id]).find(params[:question_id]) }
   end
 
-  # def create
-  #
-  # end
+  private
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
+  end
 end
