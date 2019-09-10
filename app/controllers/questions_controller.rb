a9092fde934cf4c609f6eccc5413e4c19408b44d
Brovkin Oleg <boxline@yandex.ru>
2019-09-10 21:43:49 +0300

    'поправил все замечания от Thinknetica'

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
   before_action :find_question, only: %i[show destroy del_question]
   before_action :find_test, only: %i[index create]
+
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
@@ -16,7 +17,7 @@ class QuestionsController < ApplicationController
     render plain: question.inspect
   end
 
-  def del_question
+  def destroy
     @question.destroy
     redirect_to action: :index
   end
