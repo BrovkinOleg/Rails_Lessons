679148008b704d3afbc20c33416050467dfba96a
Brovkin Oleg <boxline@yandex.ru>
2019-09-12 11:06:05 +0300

    'нет явного перехода на файл app/views/tests/index.html.erb'

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,8 @@
 class TestsController < ApplicationController
   # http://localhost:3000/tests
   def index
-    render plain: Test.all.pluck(:title)
+    @tests = Test.all
+    # render plain: Test.all.pluck(:title)
+    render file: '../views/tests/index.html.erb', layout: false
   end
 end
