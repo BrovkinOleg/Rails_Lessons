d83da7a8471bfb0115be2b8576a8edb8cd9595e9
Brovkin Oleg <boxline@yandex.ru>
2019-09-16 13:02:57 +0300

    'commit before two migrations'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_many :created_tests, class_name: 'Test', foreign_key: :admin_id, dependent: :destroy
-  has_many :tests_users, dependent: :destroy
-  has_many :tests, through: :tests_users
+  # has_many :tests_users, dependent: :destroy
+  # has_many :tests, through: :tests_users
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages
 
   validates :name, :email, presence: true
 
