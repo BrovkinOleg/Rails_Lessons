15ec368c868935f17b1ea1e958469e52c2322d75
Brovkin Oleg <boxline@yandex.ru>
2019-09-19 18:56:32 +0300

    'change gem file for has_secure_password'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,13 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  attr_reader :password
-  attr_writer :password_confirmation
+  include Auth
 
   has_many :created_tests, class_name: 'Test', foreign_key: :admin_id, dependent: :destroy
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
-  validates :name, :email, presence: true
-  validates :password, presence: true, if: Proc.new{ |u| u.password_digest.blank? }
-  validates :password, confirmation: true
+  has_secure_password
 
   def tests_list_show(level)
     tests.where(level: level)
@@ -19,24 +16,5 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == self.password_digest ? self : false
-  end
-
-  def password=(password_string)
-    if password_string.nil?
-      self.password_digest = nil
-    elsif password_string.present?
-      @password = password_string
-      self.password_digest = digest(password_string)
-    end
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
