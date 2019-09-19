9d31035e9cab32723b0dc97ddb40496bd4215966
Brovkin Oleg <boxline@yandex.ru>
2019-09-19 18:19:06 +0300

    'add permit for user name'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,16 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
+  attr_reader :password
+  attr_writer :password_confirmation
+
   has_many :created_tests, class_name: 'Test', foreign_key: :admin_id, dependent: :destroy
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
   validates :name, :email, presence: true
+  validates :password, presence: true, if: Proc.new{ |u| u.password_digest.blank? }
+  validates :password, confirmation: true
 
   def tests_list_show(level)
     tests.where(level: level)
@@ -18,6 +23,15 @@ class User < ApplicationRecord
 
   def authenticate(password_string)
     digest(password_string) == self.password_digest ? self : false
+  end
+
+  def password=(password_string)
+    if password_string.nil?
+      self.password_digest = nil
+    elsif password_string.present?
+      @password = password_string
+      self.password_digest = digest(password_string)
+    end
   end
 
   private
