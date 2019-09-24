18f3bc15071980394d6713bdc46130f482ae0e91
Brovkin Oleg <boxline@yandex.ru>
2019-09-24 12:39:56 +0300

    'add display: inline-block;'

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -7,8 +7,12 @@ class TestsController < ApplicationController
   end
 
   def start
-    current_user.tests.push(@test)
-    redirect_to current_user.test_passage(@test)
+    if current_user.nil?
+      redirect_to user_session_path, alert: 'You must login.'
+    else
+      current_user.tests.push(@test)
+      redirect_to current_user.test_passage(@test)
+    end
   end
 
   private
