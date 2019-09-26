5512a60bbca2020223433a79c685279f040ce14a
Brovkin Oleg <boxline@yandex.ru>
2019-09-26 21:49:52 +0300

    'add session_controller for flash[:novice]'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
   root 'tests#index'
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout },
+                     controllers: { sessions: 'users/sessions' }
 
   delete :logout, to: 'sessions#destroy'
 
