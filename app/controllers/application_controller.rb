4ec14102e7bbefc0c986ef0cc200f69f23b52676
Brovkin Oleg <boxline@yandex.ru>
2019-09-27 12:05:02 +0300

    'add all locales as on video'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,25 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  before_action :set_locale
 
   helper_method :log_out
 
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
   private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
   end
 
   def after_sign_in_path_for(user)
-    # flash[:greeting] = "Welcome, #{user.full_name}"
     if user.admin?
       admin_tests_path
     else
