293d4961e236151bf786c39d3643a261685f41af
Brovkin Oleg <boxline@yandex.ru>
2019-10-05 22:10:41 +0300

    'убрал у администратора прямой доступ к тестам'

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,5 @@
 class Admin < User
 
   validates :first_name, :last_name, presence: true
-  has_many :tests, foreign_key: :admin_id, dependent: :destroy
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,83 +1,48 @@
 # frozen_string_literal: true
 
 # You must delete tables in back order due to FOREIGN KEY
-Admin.delete_all
 Answer.delete_all
-TestPassage.delete_all
 Question.delete_all
+TestPassage.delete_all
 Test.delete_all
-Category.delete_all
 User.delete_all
+Category.delete_all
 
 categories = Category.create!([{ title: 'rails' }, { title: 'html' }, { title: 'java' }])
 
+admins = User.create!([{ first_name: 'Admin', last_name: 'Admin', email: 'box@box.ru',
+                         password: 'qqqqqq', type: 'Admin' }])
+
 users = User.create!([
-                       { email: 'Admin@thinknetica.ru', password: 'admin0' },
-                       { email: 'Sara@thinknetica.ru', password: 'sara00' },
-                       { email: 'Jack@thinknetica.ru', password: 'jack00' },
-                       { email: 'Bill@thinknetica.ru', password: 'bill00' },
-                       { email: 'John@thinknetica.ru', password: 'john00' }
+                       { first_name: 'Jack', last_name: 'Black', email: 'a@a.a', password: 'aaaaaa' }
                      ])
 
 tests = Test.create!([
-                       { title: 'Rails_novice', level: 0, category: categories[0], admin: users[0] },
-                       { title: 'Rails_junior', level: 1, category: categories[0], admin: users[0] },
-                       { title: 'Rails_middle', level: 2, category: categories[0], admin: users[0] },
-                       { title: 'Rails_senior', level: 3, category: categories[0], admin: users[0] },
-                       { title: 'HTML_novice', level: 0, category: categories[1], admin: users[0] },
-                       { title: 'HTML_junior', level: 1, category: categories[1], admin: users[0] },
-                       { title: 'HTML_middle', level: 2, category: categories[1], admin: users[0] },
-                       { title: 'HTML_senior', level: 3, category: categories[1], admin: users[0] },
-                       { title: 'JAVA_novice', level: 0, category: categories[2], admin: users[0] },
-                       { title: 'JAVA_junior', level: 1, category: categories[2], admin: users[0] },
-                       { title: 'JAVA_middle', level: 2, category: categories[2], admin: users[0] },
-                       { title: 'JAVA_senior', level: 3, category: categories[2], admin: users[0] }
+                       { title: 'Rails', level: 0, category: categories[0], admin: admins[0] },
+                       { title: 'HTML', level: 0, category: categories[1], admin: admins[0] },
+                       { title: 'JAVA', level: 0, category: categories[2], admin: admins[0] }
                      ])
 
 questions = Question.create!([
-                               { body: 'question_01', test: tests[0] },
-                               { body: 'question_02', test: tests[0] },
-                               { body: 'question_03', test: tests[0] },
-                               { body: 'question_04', test: tests[3] },
-                               { body: 'question_05', test: tests[4] },
-                               { body: 'question_06', test: tests[5] },
-                               { body: 'question_07', test: tests[6] },
-                               { body: 'question_08', test: tests[7] },
-                               { body: 'question_09', test: tests[8] },
-                               { body: 'question_10', test: tests[9] },
-                               { body: 'question_11', test: tests[10] },
-                               { body: 'question_12', test: tests[11] }
+                               { body: 'question_1', test: tests[0] },
+                               { body: 'question_2', test: tests[0] },
+                               { body: 'question_3', test: tests[1] },
+                               { body: 'question_4', test: tests[1] },
+                               { body: 'question_5', test: tests[2] },
+                               { body: 'question_6', test: tests[2] }
                              ])
 
 Answer.create!([
-                 { body: 'answer_01', question: questions[0] },
-                 { body: 'answer_02', question: questions[1] },
-                 { body: 'answer_03', question: questions[2] },
-                 { body: 'answer_04', question: questions[3] },
-                 { body: 'answer_05', question: questions[4] },
-                 { body: 'answer_06', question: questions[5] },
-                 { body: 'answer_07', question: questions[6] },
-                 { body: 'answer_08', question: questions[7] },
-                 { body: 'answer_09', question: questions[8] },
-                 { body: 'answer_10', question: questions[9] },
-                 { body: 'answer_11', question: questions[10] },
-                 { body: 'answer_12', question: questions[11] }
+                 { body: 'answer_11', correct: false, question: questions[0] },
+                 { body: 'answer_12', correct: false, question: questions[0] },
+                 { body: 'answer_21', correct: false, question: questions[1] },
+                 { body: 'answer_22', correct: false, question: questions[1] },
+                 { body: 'answer_31', correct: false, question: questions[2] },
+                 { body: 'answer_32', correct: false, question: questions[2] },
+                 { body: 'answer_41', correct: false, question: questions[3] },
+                 { body: 'answer_42', correct: false, question: questions[3] },
+                 { body: 'answer_51', correct: false, question: questions[4] },
+                 { body: 'answer_52', correct: false, question: questions[4] },
+                 { body: 'answer_61', correct: false, question: questions[5] },
+                 { body: 'answer_62', correct: false, question: questions[5] }
                ])
-
-tests_users = TestPassage.create!([
-                                    { user: users[0], test: tests[0], current_question: questions[0] },
-                                    { user: users[1], test: tests[1], current_question: questions[1] },
-                                    { user: users[2], test: tests[2], current_question: questions[2] },
-                                    { user: users[3], test: tests[3], current_question: questions[3] },
-                                    { user: users[4], test: tests[4], current_question: questions[4] },
-                                    { user: users[0], test: tests[5], current_question: questions[5] },
-                                    { user: users[1], test: tests[6], current_question: questions[6] },
-                                    { user: users[2], test: tests[7], current_question: questions[7] },
-                                    { user: users[3], test: tests[8], current_question: questions[8] },
-                                    { user: users[4], test: tests[9], current_question: questions[9] },
-                                    { user: users[0], test: tests[10], current_question: questions[10] },
-                                    { user: users[1], test: tests[11], current_question: questions[11] }
-                                  ])
-Admin.create!([
-                { email: 'a@a.a', password: 'aaaaaa', first_name: 'Oleg', last_name: 'Brov' }
-              ])
