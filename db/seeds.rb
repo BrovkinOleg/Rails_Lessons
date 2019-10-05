bd56462a8da2f86574c5ca645711328adff337e0
Brovkin Oleg <boxline@yandex.ru>
2019-10-05 23:00:36 +0300

    'fix seed.rb file - now it works'

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@
 
 # You must delete tables in back order due to FOREIGN KEY
 Answer.delete_all
-Question.delete_all
 TestPassage.delete_all
+Question.delete_all
 Test.delete_all
 User.delete_all
 Category.delete_all
@@ -19,17 +19,18 @@ users = User.create!([
 
 tests = Test.create!([
                        { title: 'Rails', level: 0, category: categories[0], admin: admins[0] },
-                       { title: 'HTML', level: 0, category: categories[1], admin: admins[0] },
                        { title: 'JAVA', level: 0, category: categories[2], admin: admins[0] }
                      ])
 
 questions = Question.create!([
                                { body: 'question_1', test: tests[0] },
                                { body: 'question_2', test: tests[0] },
-                               { body: 'question_3', test: tests[1] },
-                               { body: 'question_4', test: tests[1] },
-                               { body: 'question_5', test: tests[2] },
-                               { body: 'question_6', test: tests[2] }
+                               { body: 'question_3', test: tests[0] },
+                               { body: 'question_4', test: tests[0] },
+                               { body: 'question_5', test: tests[1] },
+                               { body: 'question_6', test: tests[1] },
+                               { body: 'question_7', test: tests[1] },
+                               { body: 'question_8', test: tests[1] }
                              ])
 
 Answer.create!([
@@ -44,5 +45,9 @@ Answer.create!([
                  { body: 'answer_51', correct: false, question: questions[4] },
                  { body: 'answer_52', correct: false, question: questions[4] },
                  { body: 'answer_61', correct: false, question: questions[5] },
-                 { body: 'answer_62', correct: false, question: questions[5] }
+                 { body: 'answer_62', correct: false, question: questions[5] },
+                 { body: 'answer_71', correct: false, question: questions[6] },
+                 { body: 'answer_72', correct: false, question: questions[6] },
+                 { body: 'answer_81', correct: false, question: questions[7] },
+                 { body: 'answer_82', correct: false, question: questions[7] }
                ])
