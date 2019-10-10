f516b7315c39606a93bfc817a2d60b425426245d
Brovkin Oleg <boxline@yandex.ru>
2019-10-10 21:42:34 +0300

    'now user can send email without login'

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,11 +1,13 @@
 class FeedbacksController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def new
     @feedback = Feedback.new(body: 'your question', email: 'test@guru.com')
   end
 
   def create
     @feedback = Feedback.new(feedback_params)
-    @email = current_user.email
+    @email = current_user.email unless @current_user.nil?
     if @feedback.valid?
       FeedbackMailer.send_feedback(@feedback, @email).deliver_now
       redirect_to root_path, alert: t('.success')
