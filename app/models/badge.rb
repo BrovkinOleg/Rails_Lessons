e911acddd44865f21c2d1944ef7fe9e0d7973256
Brovkin Oleg <boxline@yandex.ru>
2019-10-11 16:33:38 +0300

    'add four images for admin badges'

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -9,4 +9,10 @@ class Badge < ApplicationRecord
   has_many :users_badges
   has_many :users, through: :users_badges
   validates :title, :image, presence: true
+
+  def self.images
+    images_path = 'app/assets/images/'
+    badges = Dir.glob("#{images_path}badges/*")
+    badges.map { |badge_path| badge_path.gsub(images_path, '') }
+  end
 end
