95ed49327cf345632510c7fb95dc9b825e9e7f6e
Brovkin Oleg <boxline@yandex.ru>
2019-10-20 21:25:32 +0300

    'change badges_service to created_at'

--- app/services/badges_service.rb
+++ app/services/badges_service.rb
@@ -13,36 +13,34 @@ class BadgesService
   private
 
   def passed_success_category?(param)
-    return if @test.category.title != param
+    return if @test_passage.success? == false || @test.category.title != param
 
-    mark = 0x01 # for not repeated usage in TestPassages
     ids = Test.joins(:category).where(categories: { title: param }).ids
-    used_passages = @user.test_passages.where(test_id: ids).uniq
-    used = used_passages.select { |pass| (pass.badge_used & mark).zero? }
-    mark_set(used, mark) if @test_passage.success? && used.count >= ids.count
+    passages = @user.test_passages.where(test_id: ids).uniq
+    badge = UsersBadge.joins(:badge).where(badges: { rule_param: param }).last
+    unless badge.nil?
+      create_date = badge.created_at
+      passages = passages.select { |pass| pass.created_at > create_date }
+    end
+    passages.count >= ids.count
   end
 
   def passed_success_all_level?(param)
-    return if @test.level.to_s != param
+    return if @test_passage.success? == false || @test.level.to_s != param
 
-    mark = 0x02
     ids = Test.where(level: param).ids
-    used_passages = @user.test_passages.where(test_id: ids).uniq
-    used = used_passages.select { |pass| (pass.badge_used & mark).zero? }
-    mark_set(used, mark) if @test_passage.success? && used.uniq.count >= ids.count
+    passages = @user.test_passages.where(test_id: ids).uniq
+    badge = UsersBadge.joins(:badge).where(badges: { rule_param: param }).last
+    unless badge.nil?
+      create_date = badge.created_at
+      passages = passages.select { |pass| pass.created_at > create_date }
+    end
+    passages.count >= ids.count
   end
 
   def passed_success_on_first_try?(_no_param)
-    mark = 0x04
-    used_passage = @user.test_passages.where(test: @test)
-    mark_set(used_passage, mark) if @test_passage.success? && used_passage.count == 1
-  end
+    return unless @test_passage.success?
 
-  def mark_set(array, mark)
-    array.each do |pass|
-      pass.badge_used |= mark
-      pass.save
-    end
-    true
+    @user.test_passages.where(test: @test).count == 1
   end
 end
