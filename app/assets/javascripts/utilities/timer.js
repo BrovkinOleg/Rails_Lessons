44855fa381df7627c5416be9307b0d16465e9908
Brovkin Oleg <boxline@yandex.ru>
2019-10-22 18:34:46 +0300

    'document.querySelector.dataset can not have "_" in function name'

--- app/assets/javascripts/utilities/timer.js
+++ app/assets/javascripts/utilities/timer.js
@@ -2,7 +2,7 @@
 document.addEventListener('turbolinks:load', function() {
   let timerSeconds = document.querySelector('.my_timer');
   if (timerSeconds) {
-    let time = timerSeconds.dataset.time_left;
+    let time = timerSeconds.dataset.timeleft;
 
     let timerId = setInterval(function () { timer_work() }, 1000);
 
@@ -10,9 +10,8 @@ document.addEventListener('turbolinks:load', function() {
       if (time > 0) {
         time -= 1;
       } else {
-        // clearInterval(timerId);
-        // document.getElementById("time_over").click();
-        // document.querySelector('form').submit();
+        clearInterval(timerId);
+        document.getElementById("time_over").click();
       }
       let minutes = (time / 60).toString(10);
       timerSeconds.innerHTML = parseInt(minutes) + ':' + time % 60;
